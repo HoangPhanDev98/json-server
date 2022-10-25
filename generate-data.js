094803b18b9cede33cabddf533dd230f68dbbf4a
Ong Hoang Di Code <onghoangdicode@gmail.com>
2022-10-25 10:31:17 +0700

    Create userApi

--- generate-data.js
+++ generate-data.js
@@ -20,6 +20,28 @@ const randomCategoryList = (n) => {
 
   return categoryList;
 };
+const randomUserList = (n) => {
+  if (n <= 0) return [];
+
+  const userList = [];
+
+  //   loop and push category
+  Array.from(new Array(n)).forEach(() => {
+    const user = {
+      id: faker.datatype.uuid(),
+      name: faker.internet.userName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+      retypePassword: faker.internet.password(),
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+    };
+
+    userList.push(user);
+  });
+
+  return userList;
+};
 
 const randomProductList = (categoryList, numberOfProducts) => {
   if (numberOfProducts <= 0) return [];
@@ -52,10 +74,12 @@ const randomProductList = (categoryList, numberOfProducts) => {
   // random data
   const categoryList = randomCategoryList(4);
   const productList = randomProductList(categoryList, 5);
+  const userList = randomUserList(5);
 
   const db = {
     categories: categoryList,
     products: productList,
+    users: userList,
     profile: {
       name: "Po",
     },
@@ -65,19 +89,3 @@ const randomProductList = (categoryList, numberOfProducts) => {
     console.log("ok");
   });
 })();
-
-// export function createRandomUser(): User {
-//   return {
-//     userId: faker.datatype.uuid(),
-//     username: faker.internet.userName(),
-//     email: faker.internet.email(),
-//     avatar: faker.image.avatar(),
-//     password: faker.internet.password(),
-//     birthdate: faker.date.birthdate(),
-//     registeredAt: faker.date.past(),
-//   };
-// }
-
-// Array.from({ length: 10 }).forEach(() => {
-//   USERS.push(createRandomUser());
-// });
